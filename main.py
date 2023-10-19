e552f0cd468fee94a94348ac36a166ae9d1b2d35
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-10-19 13:29:53 -0300

    feat: now it work better

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-from __future__ import unicode_literals
-import pytube
+from pytube import YouTube
+import os
+from moviepy.editor import VideoFileClip
 
-def download_one_music(url):
-    try:
-        yt = pytube.YouTube(url)
-    except: #KEEP THAT SHIT SIMPLE ( one exception catch all )
-         print(f'Video on url {url} is unavaialable, skipping.')
-    else: 
-        print(f'Downloading video: {url}')
-        yt.streams.filter(only_audio=True).get_by_itag(140).download(output_path="./output", filename=yt.title+".mp3")
+arquivo_links = "list.txt"
+out = "./out/"
 
-def download_playlist(url):
-    try:
-        playlist = pytube.Playlist(url)
-        print(f'Downloading playlist: {playlist.title}')
-        for video in playlist.videos:
-            download_one_music(video.watch_url)
-    except:
-        print(f'Playlist on url {url} is unavaialable, skipping.')
-        
-with open("./musicList.txt") as file:
-    for line in file:
-        line = line.strip()
-        if "playlist?list=" in line:
-            download_playlist(line)
-        else:
-            download_one_music(line)
+if not os.path.exists(out):
+    os.makedirs(out)
 
+def baixar_musicas(links):
+    for link in links:
+        try:
+            yt = YouTube(link)
+            video_path = yt.streams.filter(only_audio=False).first().download(output_path=out)
+            mp4_filename = os.path.basename(video_path)
+            
+            mp3_filename = os.path.splitext(mp4_filename)[0] + ".mp3"
+            mp4_path = os.path.join(out, mp4_filename)
+            mp3_path = os.path.join(out, mp3_filename)
+            
+            # Converter o arquivo MP4 para MP3
+            video = VideoFileClip(mp4_path)
+            audio = video.audio
+            audio.write_audiofile(mp3_path)
+            
+            # Remover o arquivo MP4, se desejar
+            os.remove(mp4_path)
+            
+            print(f"{yt.title} baixada e convertida com sucesso para MP3!")
+        except Exception as e:
+            print(f"Erro ao baixar {link}: {str(e)}")
+
+def ler_links_de_arquivo(arquivo):
+    links = []
+    with open(arquivo, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith("http"):
+                links.append(line)
+    return links
+
+links = ler_links_de_arquivo(arquivo_links)
+
+baixar_musicas(links)
